35482cda1538be3a7b95db8aaa7c9c1ffeafe0b4
Miguel <mgarcia95951@gmail.com>
2019-03-04 20:18:59 -0600

    setup homepage template

--- src/components/App.js
+++ src/components/App.js
@@ -7,19 +7,19 @@ class App extends React.Component {
   render() {
     return (
       <section className="App">
-        <Container fluid={true} style={{background: 'red', height: '100vh', width: '100%', padding: '0', paddingTop: '60px'}}>
-          <Row style={{margin: '0'}} >
-          <Col xs="6">.col-6</Col>
-          <Col xs="6">
-            <Row>
-              <Col xs="6">.col-6</Col>
-              <Col xs="6">.col-6</Col>
-            </Row>
-            <Row>
-              <Col xs="6">.col-6</Col>
-              <Col xs="6">.col-6</Col>
-            </Row>
-          </Col>
+        <Container fluid={true} style={{height: '100vh', width: '100%', padding: '0', paddingTop: '56px'}}>
+          <Row style={{margin: '0', height: '80%'}} >
+            <Col xs="6" style={{background: 'red', padding: '0'}}>.col-6</Col>
+            <Col xs="6" style={{padding: '0'}}>
+              <Row style={{margin: '0', height: '50%'}}>
+                <Col style={{background: 'red', padding: '0'}} xs="6">.col-6</Col>
+                <Col style={{background: 'red', padding: '0'}} xs="6">.col-6</Col>
+              </Row>
+              <Row style={{margin: '0', height: '50%'}}>
+                <Col style={{background: 'red', padding: '0'}} xs="6">.col-6</Col>
+                <Col style={{background: 'red', padding: '0'}} xs="6">.col-6</Col>
+              </Row>
+            </Col>
           </Row>
         </Container>
       </section>
