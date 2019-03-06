ad56c628829c994d3f288a7376db87887cb74035
Miguel <mgarcia95951@gmail.com>
2019-03-06 14:10:38 -0600

    set up auth data

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,1 +1,7 @@
 import * as actionTypes from '../actions/types';
+import axios from 'axios';
+import {MOVIEDBAPI} from '../api_keys';
+
+export const login = () => {
+  // https://api.themoviedb.org/3/authentication/token/new?api_key
+}
