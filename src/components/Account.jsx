e7e532925674895ee3c99230c69cc00352ee05c6
Miguel <mgarcia95951@gmail.com>
2019-03-07 11:01:29 -0600

    get token for login and account

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -5,14 +5,27 @@ import './App.css';
 class Account extends React.Component {
   componentDidMount() {
     console.log(this.props.location.search)
+    console.log(this.getRequestToken(this.props.location.search))
+    console.log(this.getApproved(this.props.location.search))
   }
 
   getApproved = path => {
-
+    const approvedPath = path.match(new RegExp('&approved=(.*)'));
+    if (approvedPath.length > 1) {
+      return approvedPath[1];
+    } else {
+      return null
+    }
   }
 
   getRequestToken = path => {
-    
+    // const tokenPath = path.match(new RegExp('request_token=' + "(.*)" + '&'));
+    const tokenPath = path.match(new RegExp('request_token=(.*)&'));
+    if (tokenPath.length > 1) {
+      return tokenPath[1];
+    } else {
+      return null
+    }
   }
 
   render() {
