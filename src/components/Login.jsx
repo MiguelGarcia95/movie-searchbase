466b4b49fe7c7481b0f7740da34bf5be3b5a6943
Miguel <mgarcia95951@gmail.com>
2019-03-06 23:54:20 -0600

    create login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,7 +10,7 @@ class Login extends React.Component {
   render() {
     return (
       <section className="App" style={{backgroundColor: '#ddd', width: '100%', height: '100vh'}}>
-        <Jumbotron fluid style={{paddingTop: '110px'}}>
+        <Jumbotron fluid style={{paddingTop: '110px', backgroundColor: '#444', color: 'white'}}>
           <Container fluid style={{textAlign:'center'}}>
             <h1 className="display-3">Login</h1>
             <p className="lead">It'll redirect you to The MovieDB</p>
