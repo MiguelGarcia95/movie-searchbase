d4993faa35d198147d3b927bd4dacb9f48678fb2
Miguel <mgarcia95951@gmail.com>
2019-03-07 00:02:01 -0600

    create login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -17,10 +17,9 @@ class Login extends React.Component {
           </Container>
         </Jumbotron>
         <Container>
-          {/* <Row style={{height: '25%', margin: '0', marginTop: '50px'}}> */}
           <Row>
             <Col xs={{ size: '4', offset: 4 }} style={{padding: '0'}}>
-              <Button color="primary" size="lg" block>Block level button</Button>
+              <Button color="success" size="lg" block>Block level button</Button>
             </Col>
           </Row>
         </Container>
