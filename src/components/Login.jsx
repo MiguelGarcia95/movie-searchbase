2d51977a2cff5be7f0184c5b5837ba28690c5a6e
Miguel <mgarcia95951@gmail.com>
2019-03-06 23:35:05 -0600

    create login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { Jumbotron, Container } from 'reactstrap';
+
 import {getToken} from '../actions/authActions';
 
 import './App.css';
@@ -8,7 +10,12 @@ class Login extends React.Component {
   render() {
     return (
       <section className="App" style={{backgroundColor: '#ddd', width: '100%', height: '100vh'}}>
-        
+        <Jumbotron fluid style={{paddingTop: '110px'}}>
+          <Container fluid style={{textAlign:'center'}}>
+            <h1 className="display-3">Login</h1>
+            <p className="lead">It'll redirect you to The MovieDB</p>
+          </Container>
+        </Jumbotron>
       </section>
     );
   }
