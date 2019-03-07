7ebcbcd9a5f5837584f1159651fa749d495a6fd4
Miguel <mgarcia95951@gmail.com>
2019-03-07 01:23:18 -0600

    get token for login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,6 +7,10 @@ import {getToken} from '../actions/authActions';
 import './App.css';
 
 class Login extends React.Component {
+  componentDidMount() {
+    this.props.getToken();
+  }
+
   render() {
     return (
       <section className="App" style={{backgroundColor: '#ddd', width: '100%', height: '100vh'}}>
@@ -28,10 +32,16 @@ class Login extends React.Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    token: state.auth.token_id
+  }
+}
+
 const mapDispatchToProps = dispatch => {
   return {
     getToken: () => dispatch(getToken())
   }
 }
 
-export default connect(null, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
