7572548677ea54ca81a3ee1f04d65057b2c4a5fa
Miguel <mgarcia95951@gmail.com>
2019-03-08 19:33:16 -0600

    when logging in redirect

--- src/reducers/authReducers.js
+++ src/reducers/authReducers.js
@@ -3,7 +3,9 @@ import * as actionTypes from '../actions/types';
 const initialState = {
   token_id: null,
   session_id: null,
-  account: null
+  account: null,
+  redirectToLogin: false,
+  redirectToAccount: false
 };
 
 const authReducers = (state = initialState, action) => {
@@ -12,6 +14,23 @@ const authReducers = (state = initialState, action) => {
       return {
         ...state,
         account: action.payload.account
+      }
+    case actionTypes.REDIRECT_TO_ACCOUNT:
+      return {
+        ...state,
+        redirectToAccount: action.payload.redirect
+      }
+    case actionTypes.REDIRECT_TO_LOGIN:
+      return {
+        ...state,
+        redirectToLogin: action.payload.redirect
+      }
+    case actionTypes.REDIRECT_RESET:
+      return {
+        ...state,
+        redirectToLogin: action.payload.redirect,
+        redirectToAccount: action.payload.redirect
+
       }
     case actionTypes.SET_ACCOUNT:
       return {
