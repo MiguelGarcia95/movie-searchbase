54e415678aa78800e86831c7986145a1458245ee
Miguel <mgarcia95951@gmail.com>
2019-03-15 03:44:48 -0500

    fetch home movies

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -1,2 +1,11 @@
 import * as actionTypes from '../actions/types';
+import axios from 'axios';
+import {MOVIEDBAPI} from '../api_keys';
 
+export const fetchNowPlayingMovies = () => {
+  return async (dispatch) => {
+    
+    const results = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${MOVIEDBAPI}&language=en-US&page=1`)
+    console.log(results);
+  }
+}
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,4 @@
+// Auth
 export const LOGOUT  = 'LOGOUT';
 export const GET_ACCOUNT = 'GET_ACCOUNT';
 export const SET_ACCOUNT = 'SET_ACCOUNT';
@@ -6,4 +7,10 @@ export const SET_TOKEN = 'SET_TOKEN';
 export const GET_SESSION  = 'GET_SESSION';
 export const SET_SESSION = 'SET_SESSION';
 
+// Movies
 export const FETCH_POPULAR_MOVIES = 'FETCH_POPULAR_MOVIES';
+export const FETCH_LATEST_MOVIES = 'FETCH_LATEST_MOVIES';
+export const FETCH_UPCOMING_MOVIES = 'FETCH_UPCOMING_MOVIES';
+export const FETCH_NOW_PLAYING_MOVIES = 'FETCH_NOW_PLAYING_MOVIES';
+
+//TV Shows
