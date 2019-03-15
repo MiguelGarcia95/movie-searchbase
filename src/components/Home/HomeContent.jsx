9f84dd4e0c497a462af8bc8d2ea9c5c6a8f058bf
Miguel <mgarcia95951@gmail.com>
2019-03-14 22:03:14 -0500

    style components

--- src/components/Home/HomeContent.jsx
+++ src/components/Home/HomeContent.jsx
@@ -8,27 +8,9 @@ const HomeContent = () => {
   return (
     <section className="home_movies">
       <HomeSwitch />
-      <section className="home_slider">
-        <h3 className="home_slider_name">Top Movies</h3>
-        <section className="home_slider_container">
-          <ContentSlider />
-        </section>
-      </section>
-
-      <section className="home_slider">
-        <h3 className="home_slider_name">Top Movies</h3>
-        <section className="home_slider_container">
-          <ContentSlider />
-        </section>
-      </section>
-
-      <section className="home_slider">
-        <h3 className="home_slider_name">Top Movies</h3>
-        <section className="home_slider_container">
-          <ContentSlider />
-        </section>
-      </section>
-
+      <ContentSlider />
+      <ContentSlider />
+      <ContentSlider />
     </section>
   )
 }
--- src/components/layout/ContentSlider.jsx
+++ src/components/layout/ContentSlider.jsx
@@ -13,70 +13,75 @@ class ContentSlider extends React.Component {
     };
 
     return (
-      <div>
-        <Slider {...settings}>
+      <section className="home_slider">
+        <h3 className="home_slider_name">Top Movies</h3>
+        <section className="home_slider_container">
           <div>
-            <h3 style={{textAlign: 'center'}}>1</h3>
-            <h3 style={{textAlign: 'center'}}>1</h3>
-            <h3 style={{textAlign: 'center'}}>1</h3>
-            <h3 style={{textAlign: 'center'}}>1</h3>
+            <Slider {...settings}>
+              <div>
+                <h3 style={{textAlign: 'center'}}>1</h3>
+                <h3 style={{textAlign: 'center'}}>1</h3>
+                <h3 style={{textAlign: 'center'}}>1</h3>
+                <h3 style={{textAlign: 'center'}}>1</h3>
+              </div>
+              <div >
+                <h3 style={{textAlign: 'center'}}>2</h3>
+                <h3 style={{textAlign: 'center'}}>2</h3>
+                <h3 style={{textAlign: 'center'}}>2</h3>
+                <h3 style={{textAlign: 'center'}}>2</h3>
+              </div>
+              <div >
+                <h3 style={{textAlign: 'center'}}>3</h3>
+                <h3 style={{textAlign: 'center'}}>3</h3>
+                <h3 style={{textAlign: 'center'}}>3</h3>
+                <h3 style={{textAlign: 'center'}}>3</h3>
+              </div>
+              <div >
+                <h3 style={{textAlign: 'center'}}>4</h3>
+                <h3 style={{textAlign: 'center'}}>4</h3>
+                <h3 style={{textAlign: 'center'}}>4</h3>
+                <h3 style={{textAlign: 'center'}}>4</h3>
+              </div>
+              <div >
+                <h3 style={{textAlign: 'center'}}>5</h3>
+                <h3 style={{textAlign: 'center'}}>5</h3>
+                <h3 style={{textAlign: 'center'}}>5</h3>
+                <h3 style={{textAlign: 'center'}}>5</h3>
+              </div>
+              <div >
+                <h3 style={{textAlign: 'center'}}>6</h3>
+                <h3 style={{textAlign: 'center'}}>6</h3>
+                <h3 style={{textAlign: 'center'}}>6</h3>
+                <h3 style={{textAlign: 'center'}}>6</h3>
+              </div>
+              <div >
+                <h3 style={{textAlign: 'center'}}>7</h3>
+                <h3 style={{textAlign: 'center'}}>7</h3>
+                <h3 style={{textAlign: 'center'}}>7</h3>
+                <h3 style={{textAlign: 'center'}}>7</h3>
+              </div>
+              <div >
+                <h3 style={{textAlign: 'center'}}>8</h3>
+                <h3 style={{textAlign: 'center'}}>8</h3>
+                <h3 style={{textAlign: 'center'}}>8</h3>
+                <h3 style={{textAlign: 'center'}}>8</h3>
+              </div>
+              <div >
+                <h3 style={{textAlign: 'center'}}>9</h3>
+                <h3 style={{textAlign: 'center'}}>9</h3>
+                <h3 style={{textAlign: 'center'}}>9</h3>
+                <h3 style={{textAlign: 'center'}}>9</h3>
+              </div>
+              <div >
+                <h3 style={{textAlign: 'center'}}>10</h3>
+                <h3 style={{textAlign: 'center'}}>10</h3>
+                <h3 style={{textAlign: 'center'}}>10</h3>
+                <h3 style={{textAlign: 'center'}}>10</h3>
+              </div>
+            </Slider>
           </div>
-          <div >
-            <h3 style={{textAlign: 'center'}}>2</h3>
-            <h3 style={{textAlign: 'center'}}>2</h3>
-            <h3 style={{textAlign: 'center'}}>2</h3>
-            <h3 style={{textAlign: 'center'}}>2</h3>
-          </div>
-          <div >
-            <h3 style={{textAlign: 'center'}}>3</h3>
-            <h3 style={{textAlign: 'center'}}>3</h3>
-            <h3 style={{textAlign: 'center'}}>3</h3>
-            <h3 style={{textAlign: 'center'}}>3</h3>
-          </div>
-          <div >
-            <h3 style={{textAlign: 'center'}}>4</h3>
-            <h3 style={{textAlign: 'center'}}>4</h3>
-            <h3 style={{textAlign: 'center'}}>4</h3>
-            <h3 style={{textAlign: 'center'}}>4</h3>
-          </div>
-          <div >
-            <h3 style={{textAlign: 'center'}}>5</h3>
-            <h3 style={{textAlign: 'center'}}>5</h3>
-            <h3 style={{textAlign: 'center'}}>5</h3>
-            <h3 style={{textAlign: 'center'}}>5</h3>
-          </div>
-          <div >
-            <h3 style={{textAlign: 'center'}}>6</h3>
-            <h3 style={{textAlign: 'center'}}>6</h3>
-            <h3 style={{textAlign: 'center'}}>6</h3>
-            <h3 style={{textAlign: 'center'}}>6</h3>
-          </div>
-          <div >
-            <h3 style={{textAlign: 'center'}}>7</h3>
-            <h3 style={{textAlign: 'center'}}>7</h3>
-            <h3 style={{textAlign: 'center'}}>7</h3>
-            <h3 style={{textAlign: 'center'}}>7</h3>
-          </div>
-          <div >
-            <h3 style={{textAlign: 'center'}}>8</h3>
-            <h3 style={{textAlign: 'center'}}>8</h3>
-            <h3 style={{textAlign: 'center'}}>8</h3>
-            <h3 style={{textAlign: 'center'}}>8</h3>
-          </div>
-          <div >
-            <h3 style={{textAlign: 'center'}}>9</h3>
-            <h3 style={{textAlign: 'center'}}>9</h3>
-            <h3 style={{textAlign: 'center'}}>9</h3>
-            <h3 style={{textAlign: 'center'}}>9</h3>
-          </div>
-          <div >
-            <h3 style={{textAlign: 'center'}}>10</h3>
-            <h3 style={{textAlign: 'center'}}>10</h3>
-            <h3 style={{textAlign: 'center'}}>10</h3>
-            <h3 style={{textAlign: 'center'}}>10</h3>
-          </div>
-        </Slider>
-      </div>
+        </section>
+      </section>
     );
   }
 }
