9b4092ce79410a7c6e252ae7f6d941c8fb1af44e
Miguel <mgarcia95951@gmail.com>
2019-03-15 04:16:35 -0500

    fetch home movies

--- src/reducers/movieReducers.js
+++ src/reducers/movieReducers.js
@@ -11,19 +11,23 @@ const movieReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.FETCH_POPULAR_MOVIES:
       return {
-        ...state
+        ...state,
+        popularMovies: action.payload.popularMovies
       }
     case actionTypes.FETCH_NOW_PLAYING_MOVIES:
       return {
-        ...state
+        ...state,
+        nowPlayingMovies: action.payload.nowPlayingMovies
       }
     case actionTypes.FETCH_LATEST_MOVIES:
       return {
-        ...state
+        ...state,
+        latestsMovies: action.payload.latestsMovies
       }
     case actionTypes.FETCH_UPCOMING_MOVIES:
       return {
-        ...state
+        ...state,
+        upcomingMovies: action.payload.upcomingMovies
       }
     default:
       return state;
