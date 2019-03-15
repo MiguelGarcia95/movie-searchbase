8d1e255fddd0061dd66c5d16cd2867f5d37dfe30
Miguel <mgarcia95951@gmail.com>
2019-03-15 03:46:28 -0500

    fetch home movies

--- src/reducers/movieReducers.js
+++ src/reducers/movieReducers.js
@@ -1,12 +1,27 @@
 import * as actionTypes from '../actions/types';
 
 const initialState = {
-
+  upcomingMovies: [],
+  latestsMovies: [],
+  nowPlayingMovies: [],
+  popularMovies: []
 };
 
 const movieReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.FETCH_POPULAR_MOVIES:
+      return {
+        ...state
+      }
+    case actionTypes.FETCH_NOW_PLAYING_MOVIES:
+      return {
+        ...state
+      }
+    case actionTypes.FETCH_LATEST_MOVIES:
+      return {
+        ...state
+      }
+    case actionTypes.FETCH_UPCOMING_MOVIES:
       return {
         ...state
       }
