240c52cf115484bf0bf8210bc7ce1688faada8c7
Miguel <mgarcia95951@gmail.com>
2019-03-15 23:22:42 -0500

    display movie slide

--- src/components/layout/Slide.jsx
+++ src/components/layout/Slide.jsx
@@ -11,6 +11,16 @@ const getTitle = (movie, type) => {
   return title;
 }
 
+const getYear = (movie, type) => {
+  let year = '';
+  if (type === 'movies') {
+    year = movie.release_date.slice(0,4);
+  } else {
+    year = movie.first_air_date.slice(0,4);
+  }
+  return year;
+}
+
 const Slide = ({movie, type}) => {
   return (
     <section className="search_result">
@@ -19,7 +29,7 @@ const Slide = ({movie, type}) => {
         <p className="search_result_movie_rating">{movie.vote_average}</p>  
       </section>
       <section className="search_result_movie_data">
-          <section className="search_result_movie_meta"><p>2015 / Genre</p></section>  
+          <section className="search_result_movie_meta"><p>{getYear(movie, type)} / Genre</p></section>  
           <section className="search_result_movie_title"><p>{getTitle(movie, type)}</p></section>  
       </section>
     </section>
