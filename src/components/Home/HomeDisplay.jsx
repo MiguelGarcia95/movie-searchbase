a79eca8a87e48fe4dd20493aabc050b9584c0d30
Miguel <mgarcia95951@gmail.com>
2019-03-16 20:09:12 -0500

    add home display content

--- src/components/Home/HomeDisplay.jsx
+++ src/components/Home/HomeDisplay.jsx
@@ -1,71 +1,6 @@
 import React from 'react';
 import './style/css/HomeDisplay.css';
 
-const displayBottomCol = movies => {
-  let newMovies = movies.slice(5, 11);
-  return newMovies.map(movie => {
-    return (
-      <section key={movie.id} className="home_display_bottom_col">
-        <section className="display_content">
-          <section className="content_image"></section>
-          <section className="content_data">
-            <section className="content_title"><h1>Title Here</h1></section>
-            <section className="content_details"><p>Movies data goes here here here here here</p></section>
-          </section>
-        </section>
-      </section>
-    )
-  })
-}
-
-const displayTopInnerCol = movies => {
-  let newMovies = movies.slice(1, 3);
-  return newMovies.map(movie => {
-    return (
-      <section key={movie.id} className="home_display_inner_col">
-        <section className="display_content">
-          <section className="content_image"></section>
-          <section className="content_data">
-            <section className="content_title"><h1>Title Here</h1></section>
-            <section className="content_details"><p>Movies data goes here here here here here</p></section>
-          </section>
-        </section>
-      </section>
-    )
-  })
-}
-
-const displayBottomInnerCol = movies => {
-  let newMovies = movies.slice(3, 5);
-  return newMovies.map(movie => {
-    return (
-      <section key={movie.id} className="home_display_inner_col">
-        <section className="display_content">
-          <section className="content_image"></section>
-          <section className="content_data">
-            <section className="content_title"><h1>Title Here</h1></section>
-            <section className="content_details"><p>Movies data goes here here here here here</p></section>
-          </section>
-        </section>
-      </section>
-    )
-  })
-}
-
-const displayHomeCol = movies => {
-  return (
-    <section className="home_display_col">
-      <section className="display_content">
-        <section className="content_image"></section>
-        <section className="content_data">
-          <section className="content_title"><h1>Title Here</h1></section>
-          <section className="content_details"><p>Movies data goes here here here here here</p></section>
-        </section>
-      </section>
-    </section>
-  )
-}
-
 const getTitle = (movie, type) => {
   let title = '';
   if (type === 'movies') {
@@ -102,22 +37,87 @@ const getGenre = (movie, genres) => {
   return genre;
 }
 
+const displayBottomCol = (movies, genres, fetchType) => {
+  let newMovies = movies.slice(5, 11);
+  return newMovies.map(movie => {
+    return (
+      <section key={movie.id} className="home_display_bottom_col">
+        <section className="display_content">
+          <section className="content_image"></section>
+          <section className="content_data">
+            <section className="content_title"><h1>{getTitle(movie, fetchType)}</h1></section>
+            <section className="content_details"><p>Movies data goes here here here here here</p></section>
+          </section>
+        </section>
+      </section>
+    )
+  })
+}
+
+const displayTopInnerCol = (movies, genres, fetchType) => {
+  let newMovies = movies.slice(1, 3);
+  return newMovies.map(movie => {
+    return (
+      <section key={movie.id} className="home_display_inner_col">
+        <section className="display_content">
+          <section className="content_image"></section>
+          <section className="content_data">
+            <section className="content_title"><h1>{getTitle(movie, fetchType)}</h1></section>
+            <section className="content_details"><p>Movies data goes here here here here here</p></section>
+          </section>
+        </section>
+      </section>
+    )
+  })
+}
+
+const displayBottomInnerCol = (movies, genres, fetchType) => {
+  let newMovies = movies.slice(3, 5);
+  return newMovies.map(movie => {
+    return (
+      <section key={movie.id} className="home_display_inner_col">
+        <section className="display_content">
+          <section className="content_image"></section>
+          <section className="content_data">
+            <section className="content_title"><h1>{getTitle(movie, fetchType)}</h1></section>
+            <section className="content_details"><p>Movies data goes here here here here here</p></section>
+          </section>
+        </section>
+      </section>
+    )
+  })
+}
+
+const displayHomeCol = (movies, genres, fetchType) => {
+  return (
+    <section className="home_display_col">
+      <section className="display_content">
+        <section className="content_image"></section>
+        <section className="content_data">
+          <section className="content_title"><h1>{getTitle(movies[0], fetchType)}</h1></section>
+          <section className="content_details"><p>Movies data goes here here here here here</p></section>
+        </section>
+      </section>
+    </section>
+  )
+}
+
 const HomeDisplay = ({movies, genres, fetchType}) => {
   return (
     <section className='home_display'>
       <section className="home_display_row">
-        {movies && displayHomeCol(movies)}
+        {movies.length > 0 && displayHomeCol(movies, genres, fetchType)}
         <section className="home_display_col">
           <section className="home_display_inner_row">
-            {movies && displayTopInnerCol(movies)}
+            {movies && displayTopInnerCol(movies, genres, fetchType)}
           </section>  
           <section className="home_display_inner_row">
-            {movies && displayBottomInnerCol(movies)}
+            {movies && displayBottomInnerCol(movies, genres, fetchType)}
           </section>  
         </section>
       </section>
       <section className="home_display_bottom_row">
-        {movies.length > 0 && displayBottomCol(movies)}
+        {movies.length && displayBottomCol(movies, genres, fetchType)}
       </section>
     </section>
   )
