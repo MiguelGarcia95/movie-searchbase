20e402f300fe1d4189ffade9fd9459a4742c9187
Miguel <mgarcia95951@gmail.com>
2019-03-19 18:40:28 -0500

    work on search feature

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 import {MOVIEDBAPI} from '../api_keys';
 
 export const fetchSearchResults = (searchQuery, page) => {
-  return (dispatch) => {
-    console.log(searchQuery)
-    console.log(page)
-
+  return async (dispatch) => {
+    const results = await axios.get(`https://api.themoviedb.org/3/search/company?api_key=${MOVIEDBAPI}&query=${searchQuery}&page=${page}`);
+    console.log(results.data.page);
+    console.log(results.data.total_pages);
+    console.log(results.data.total_results);
+    console.log(results.data.results);
     dispatch({
       type: actionTypes.FETCH_SEARCH_RESULTS,
       payload: {
