69127ee4f04bc58c81278fd2939c6186bb94d64a
Miguel <mgarcia95951@gmail.com>
2019-03-18 23:19:28 -0500

    make homedisplay responsive

--- src/components/layout/MyFooter.jsx
+++ src/components/layout/MyFooter.jsx
@@ -11,7 +11,9 @@ const MyFooter = () => {
           <a className="nav_icon" href="/theme/account.hmtl"><i className="far fa-2x fa-user-circle"></i> Account</a>
           <input className='navbar_search' type='text' placeholder="Search" />
         </section>
-        <img src="http://localhost:3000/images/logos/408x161-powered-by-rectangle-green-bb4301c10ddc749b4e79463811a68afebeae66ef43d17bcfd8ff0e60ded7ce99.png" alt="" />
+        <a href='https://www.themoviedb.org/'>
+          <img src="http://localhost:3000/images/logos/408x161-powered-by-rectangle-green-bb4301c10ddc749b4e79463811a68afebeae66ef43d17bcfd8ff0e60ded7ce99.png" alt="" />
+        </a>
       </section>
       <section className="footer_meta">
         <a className="nav_icon" id='meta_left' href="mailto:miguel@miguelgarcia.app"><i className="fas fa-envelope-open"></i> miguel@miguelgarcia.app</a> 
