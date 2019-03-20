6fe2bcd8ab38c99fdb52c581eac2c6d30e28e2f8
Miguel <mgarcia95951@gmail.com>
2019-03-19 21:49:13 -0500

    work on search feature

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -20,13 +20,33 @@ export const fetchSearchResults = (searchQuery, page) => {
 
 export const fetchMoviesSearch = (searchQuery, page) => {
   return async (dispatch) => {
-
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIEDBAPI}&language=en-US&query=${searchQuery}&page=${page}&include_adult=false`;
+    const results = await axios.get(url);
+    dispatch({
+      type: actionTypes.SEARCH_MOVIE_RESULTS,
+      payload: {
+        moviesSearchResults: results.data.results,
+        moviesCurrentPage: results.data.page,
+        moviesTotalPages: results.data.total_pages,
+        moviesTotalResults: results.data.total_results
+      }
+    })
   }
 }
 
-export const fetchResultsSearch = (searchQuery, page) => {
+export const fetchShowSearch = (searchQuery, page) => {
   return async (dispatch) => {
-    
+    const url = `https://api.themoviedb.org/3/search/tv?api_key=${MOVIEDBAPI}&language=en-US&query=${searchQuery}&page=${page}`;
+    const results = await axios.get(url);
+    dispatch({
+      type: actionTypes.SEARCH_SHOW_RESULTS,
+      payload: {
+        showsSearchResults: results.data.results,
+        showsCurrentPage: results.data.page,
+        showsTotalPages: results.data.total_pages,
+        showsTotalResults: results.data.total_results
+      }
+    })
   }
 }
 
