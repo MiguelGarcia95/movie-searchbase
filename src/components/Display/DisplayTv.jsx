d272d017ced8cdde71ee65705101ee7c65723d02
Miguel <mgarcia95951@gmail.com>
2019-03-22 13:35:03 -0500

    get movie and show display

--- src/components/Display/DisplayTv.jsx
+++ src/components/Display/DisplayTv.jsx
@@ -9,13 +9,26 @@ class DisplayTv extends React.Component {
     this.props.fetchShow(this.props.match.params.showId);
   }
 
+  getImage = () => {
+    let image = '';
+    if (this.props.backdrop_path) {
+      image = `https://image.tmdb.org/t/p/original${this.props.backdrop_path}`;
+    } else {
+      image = '/images/movie/movie_bg.jpeg';
+    }
+    return image;
+  }
+
   render() {
     const {currentShow} = this.props;
+    const imageStyle = {
+      backgroundImage: `url(${this.getImage()})`
+    }
     return (
       <section className="display_movie">
         {currentShow && (
           <section className="display_movie_header">
-            <section className="display_movie_header_image"></section>
+            <section className="display_movie_header_image" style={imageStyle}></section>
             <section className="display_movie_header_data">
               <h1 className="title">Title Here</h1>
               <p className="meta">Genre genre genre </p>
