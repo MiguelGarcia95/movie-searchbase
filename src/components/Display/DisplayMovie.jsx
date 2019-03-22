12f2a8c00d3e8a85dbf73aa37a0ba30e2837fd86
Miguel <mgarcia95951@gmail.com>
2019-03-22 00:15:30 -0500

    work on search feature

--- src/components/Display/DisplayMovie.jsx
+++ src/components/Display/DisplayMovie.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import './style/css/Display.css';
 
 class DisplayMovie extends React.Component {
+  componentDidMount() {
+    console.log(this.props.match.params.movieId);
+  }
+
   render() {
     return (
       <section className="display_movie">
