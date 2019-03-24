4546cbcd86c7ff247887ce2db1844ee0fbf77864
Miguel <mgarcia95951@gmail.com>
2019-03-23 20:50:52 -0500

    set movie display

--- src/components/layout/CastSlide.jsx
+++ src/components/layout/CastSlide.jsx
@@ -3,9 +3,9 @@ import './style/css/CastSlide.css';
 import {Link} from 'react-router-dom';
 
 
-const CastSlide = ({cast}) => {
+const CastSlide = ({character}) => {
   const imageStyle = {
-    backgroundImage: `url(https://image.tmdb.org/t/p/original${cast.profile_path})`,
+    backgroundImage: `url(https://image.tmdb.org/t/p/original${character.profile_path})`,
     backgroundSize: 'cover',
     backgroundPosition: 'center center',
     backgroundRepeat: 'no-repeat'
@@ -15,7 +15,7 @@ const CastSlide = ({cast}) => {
     <section className="cast_character">
       <section className="cast_image" style={imageStyle}></section>
       <section className="cast_data">
-        <p className="name" >{cast.name}</p>
+        <p className="name" >{character.name}</p>
       </section>
     </section>
   );
--- src/components/layout/CastSlider.jsx
+++ src/components/layout/CastSlider.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 import Slider from "react-slick";
-// import Slide from './Slide';
-
-// const displayCast = (cast) => {
-//   return movies.map(movie => {
-//     return <Slide key={movie.id} movie={movie} type={type} genres={genres} />
-//   })
-// }
+import CastSlide from './CastSlide';
 
 const displayCast = cast => {
   return cast.slice(0, 8).map(character => {
@@ -16,14 +10,7 @@ const displayCast = cast => {
       backgroundPosition: 'center center',
       backgroundRepeat: 'no-repeat'
     }
-    return (
-      <section className="cast_character" key={character.cast_id}>
-        <section className="cast_image" style={imageStyle}></section>
-        <section className="cast_data">
-          <p className="name" >{character.name}</p>
-        </section>
-      </section>
-    )
+    return <CastSlide key={character.id} character={character}/>
   })
 }
 
