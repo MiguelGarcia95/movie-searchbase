38a646aedad749d0d9472ae0bd8465d447f6e7fd
Miguel <mgarcia95951@gmail.com>
2019-03-24 22:25:19 -0500

    set show display

--- src/components/Display/DisplayTv.jsx
+++ src/components/Display/DisplayTv.jsx
@@ -3,12 +3,13 @@ import {connect} from 'react-redux';
 import DisplayHeader from './DisplayHeader';
 import CastSlider from '../layout/CastSlider';
 import ContentSlider from '../layout/ContentSlider';
-import {fetchShow, fetchShowCredits, fetchShowReviews, fetchSimilarShows} from '../../actions/tvShowActions'; 
+import {fetchShow, fetchShowCredits, fetchShowReviews, fetchSimilarShows, fetchShowGenres} from '../../actions/tvShowActions'; 
 import {castSliderSettings, movieTvSliderSettings} from '../../utils/settings';
 import './style/css/Display.css';
 
 class DisplayTv extends React.Component {
   componentDidMount() {
+    this.props.fetchShowGenres()
     this.fetchShowData();
   }
 
@@ -25,59 +26,67 @@ class DisplayTv extends React.Component {
     this.props.fetchSimilarShows(this.props.match.params.showId);
   }
 
-  getImage = () => {
-    let image = '';
-    if (this.props.currentShow) {
-      if (this.props.currentShow.backdrop_path) {
-        image = `https://image.tmdb.org/t/p/original${this.props.currentShow.backdrop_path}`;
-      } else {
-        image = '/images/movie/movie_bg.jpeg';
-      }
+  displayCast = cast => {
+    return <CastSlider cast={cast} settings={castSliderSettings} />
+  }
+
+  displaySimilarShows = (movies, genres) => {
+    return  <ContentSlider movies={movies} genres={genres} type='shows' settings={movieTvSliderSettings} />
+  }
+
+  displayReviews = reviews => {
+    if (reviews.length > 0) {
+      return reviews.map(review => {
+        return (
+          <section key={review.id} className="review">
+            <section className="review_username"><p><span>From: </span> <a href={review.url}>{review.author}</a></p></section>
+            <section className="review_content"><p>{review.content}</p></section>
+          </section>
+        )
+      })
     } else {
-      image = '/images/movie/movie_bg.jpeg';
+      return <section className='no_reviews'><h2>No Reviews ...</h2></section>
     }
-    return image;
   }
 
-  displayCompanies = (companies) => {
-    return companies.map(company => {
-      return (
-        <section key={company.name} title={company.name} className="company">
-          <img src={`https://image.tmdb.org/t/p/original${company.logo_path}`} alt={`${company.name} logo`}/>
-        </section>
-      )
-    })
-  }
+  // getImage = () => {
+  //   let image = '';
+  //   if (this.props.currentShow) {
+  //     if (this.props.currentShow.backdrop_path) {
+  //       image = `https://image.tmdb.org/t/p/original${this.props.currentShow.backdrop_path}`;
+  //     } else {
+  //       image = '/images/movie/movie_bg.jpeg';
+  //     }
+  //   } else {
+  //     image = '/images/movie/movie_bg.jpeg';
+  //   }
+  //   return image;
+  // }
 
-  displayGenres = (genres) => {
-    return genres.map(genre => {
-      return <span key={genre.id} className="genre">{genre.name}</span>
-    })
-  }
+  // displayCompanies = (companies) => {
+  //   return companies.map(company => {
+  //     return (
+  //       <section key={company.name} title={company.name} className="company">
+  //         <img src={`https://image.tmdb.org/t/p/original${company.logo_path}`} alt={`${company.name} logo`}/>
+  //       </section>
+  //     )
+  //   })
+  // }
+
+  // displayGenres = (genres) => {
+  //   return genres.map(genre => {
+  //     return <span key={genre.id} className="genre">{genre.name}</span>
+  //   })
+  // }
 
   render() {
-    const {currentShow} = this.props;
-    const imageStyle = {
-      backgroundImage: `url(${this.getImage()})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center center',
-      backgroundRepeat: 'no-repeat'
-    }
+    const {currentShow, currentShowCredits, currentShowReviews, similarShows, showGenres} = this.props;
+
     return (
       <section className="display_movie">
-        {currentShow && (
+        {currentShow && currentShow.id === Number(this.props.match.params.showId) && (
           <React.Fragment>
-            <section className="display_movie_header">
-              <section className="display_movie_header_image" style={imageStyle}></section>
-              <section className="display_movie_header_data">
-                <h1 className="title">{currentShow.original_name}</h1>
-                <p className="meta">{this.displayGenres(currentShow.genres)}</p>
-                <h4 className="description">{currentShow.overview}</h4>
-                <section className="companies">
-                  {this.displayCompanies(currentShow.networks)}
-                </section>
-              </section>
-            </section>
+            <DisplayHeader currentMovie={currentShow} />
             <section className="display_movie_data">
               <section className="display_movie_data_cast">
               </section>
@@ -91,6 +100,24 @@ class DisplayTv extends React.Component {
           </React.Fragment>
         )}
       </section>
+
+        //  <React.Fragment>
+        //    <DisplayHeader currentMovie={currentMovie} />
+        //    <section className="display_movie_data">
+        //      <section className="display_movie_data_cast">
+        //        <h2>Cast {currentMoviesCredits && <small>({currentMoviesCredits.cast.slice(0, 20).length})</small>}</h2>
+        //        {currentMoviesCredits && this.displayCast(currentMoviesCredits.cast)}
+        //      </section>
+        //      <section className="display_movie_data_similar">
+        //        <h2>Similar Movies  {similarMovies && <small>({similarMovies.length})</small>} </h2>
+        //        {similarMovies && movieGenres && this.displaySimilarMovies(similarMovies, movieGenres)}
+        //      </section>
+        //      <section className="display_movie_data_reviews">
+        //        <h2>Reviews  {currentMoviesReviews && <small>({currentMoviesReviews.length})</small>}</h2>
+        //        {currentMoviesReviews && this.displayReviews(currentMoviesReviews)}
+        //      </section>
+        //    </section>
+        //  </React.Fragment>
     );
   }
 }
@@ -100,7 +127,8 @@ const mapStateToProps = state => {
     currentShow: state.shows.currentShow,
     currentShowCredits: state.shows.currentShowCredits,
     currentShowReviews: state.shows.currentShowReviews,
-    similarShows: state.shows.similarShows
+    similarShows: state.shows.similarShows,
+    showGenres: state.shows.showGenres
   }
 }
 
@@ -109,7 +137,8 @@ const mapDispatchToProps = dispatch => {
     fetchShow: id => dispatch(fetchShow(id)),
     fetchShowCredits: id => dispatch(fetchShowCredits(id)),
     fetchShowReviews: id => dispatch(fetchShowReviews(id)),
-    fetchSimilarShows: id => dispatch(fetchSimilarShows(id))
+    fetchSimilarShows: id => dispatch(fetchSimilarShows(id)),
+    fetchShowGenres: () => dispatch(fetchShowGenres())
   }
 }
 
