eb0d70a52d56da700c2386e2cbefca86f75501f1
Miguel <mgarcia95951@gmail.com>
2019-03-24 23:56:06 -0500

    set show display

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,15 @@ import authReducers from './authReducers';
 import tvShowReducers from './tvShowReducers';
 import searchReducers from './searchReducers';
 import settingsReducers from './settingsReducers';
+import peopleReducers from './peopleReducers';
 
 const rootReducer = combineReducers({
   movies: movieReducers,
   shows:  tvShowReducers,
   auth: authReducers,
   results: searchReducers,
-  settings: settingsReducers
+  settings: settingsReducers,
+  people: peopleReducers
 });
 
 export default rootReducer;
