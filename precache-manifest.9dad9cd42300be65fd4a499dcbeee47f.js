2e3a91b83ac2a6d8a95f678355dfd1cb1a6ecbf0
Miguel <mgarcia95951@gmail.com>
2019-04-01 00:38:36 -0500

    Updates

---
+++ precache-manifest.9dad9cd42300be65fd4a499dcbeee47f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/movie-searchbase/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/movie-searchbase/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/movie-searchbase/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/movie-searchbase/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "493d70e57f38bb6cae12",
+    "url": "/movie-searchbase/static/js/runtime~main.493d70e5.js"
+  },
+  {
+    "revision": "e5067ec54d41534a19c0",
+    "url": "/movie-searchbase/static/js/main.e5067ec5.chunk.js"
+  },
+  {
+    "revision": "9aff3fd58650756fd370",
+    "url": "/movie-searchbase/static/js/2.9aff3fd5.chunk.js"
+  },
+  {
+    "revision": "e5067ec54d41534a19c0",
+    "url": "/movie-searchbase/static/css/main.3b773220.chunk.css"
+  },
+  {
+    "revision": "9aff3fd58650756fd370",
+    "url": "/movie-searchbase/static/css/2.5fb07535.chunk.css"
+  },
+  {
+    "revision": "ad5052e78869d27dd5a22c55512485e3",
+    "url": "/movie-searchbase/index.html"
+  }
+];
